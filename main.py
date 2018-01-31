b63aea7ff8d56406971cf32cc5a2569509012c57
Remillardj <jaryd.remillard@gmail.com>
2018-01-30 22:02:43 -0800

    Adding more arguments, with help text

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-from modules import *
+import module.logging_config as log
 
 def banner():
     print ("---------------------------------")
@@ -11,21 +11,20 @@ def banner():
     print ("#################################")
 
 if (__name__ == "__main__"):
+    parser = argparse.ArgumentParser(description='pyDBLoader Help Menu')
+    parser.add_argument('-c', '--config', default='./config/logger_config.yml',
+                        help='Load configuration file to load user-specified databases, with information needed for connection.')
+    parser.add_argument('-l', '--log', default='./logs/pydbloader-main.log',
+                        help='[DEPRECATED: Log the program. Default is located in ./logs/pydbloader-main.log')
+    parser.add_argument('-v', '--verbose', help='[DEPRECATED] Allow for more verbose actions', default=False)
+    args = parser.parse_args()
+
+    '''
 	parser = argparse.ArgumentParser(description='pyDBLoader help menu')
 	parser.add_argument('--sqlite3', help='Insert your database into an SQLite3 database')
     parser.add_argument('--filepath', help='The location of your database')
 	args = parser.parse_args()
+    '''
 
+# Call banner
 banner()
-
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-setup(name = 'pydbloader',
-      version = '0.0.2',
-      description = 'A python database loader for various relational databases',
-      url = 'https://github.com/Remillardj/pyDBLoader',
-      author = 'Jaryd Remillard',
-      license = 'Apache',
-      install_requires = [ requirements ],
-)
