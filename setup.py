363220ba57e9dab63024f229f77fbfb87f423bcb
Remillardj <jaryd.remillard@gmail.com>
2018-01-30 22:15:25 -0800

    Updated setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(name = 'pydbloader',
+      version = '0.0.2',
+      description = 'A python database loader for various relational databases',
+      url = 'https://github.com/Remillardj/pyDBLoader',
+      author = 'Jaryd Remillard',
+      license = 'Apache',
+      install_requires = [ requirements ],
+)
